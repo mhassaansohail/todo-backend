507966bc8f2a39ac4548bfe7872343c9670c34e9
Hassaan Sohail <hassaan.sohail@carbonteq.com>
2024-02-14 19:27:18 +0500

    changes.

--- src/APP/Application/auth/AuthService.ts
+++ src/APP/Application/auth/AuthService.ts
@@ -29,10 +29,6 @@ export class AuthService {
     async loginWithOAuth(): Promise<Ok<string> | Err<Error>> {
         try {
             const consentScreenUrlResult = await this.oAuthService.generateAuthURL(['profile', 'email']);
-            // if (consentScreenUrlResult.isErr()) {
-            //     const { message } = consentScreenUrlResult.unwrapErr();
-            //     return Err(new Error(message));
-            // }
             return Ok(consentScreenUrlResult.unwrap())
         } catch (error) {
             return Err(new Error("Invalid credentials."));
@@ -43,18 +39,10 @@ export class AuthService {
         try {
             const userCredentials = new Credentials(userName, password);
             const isValidUserResult = await this.verifyUser(userCredentials._username, userCredentials._password);
-            // if (isValidUserResult.isErr()) {
-            //     const { message } = isValidUserResult.unwrapErr();
-            //     return Err(new Error(message));
-            // }
             if (!isValidUserResult.unwrap()) {
                 return Err(new Error("Invalid username or password."));
             }
             const tokenResult = await this.jwtService.genrateTokenFromParam(userName);
-            // if (tokenResult.isErr()) {
-            //     const { message } = tokenResult.unwrapErr()
-            //     return Err(new Error(message));
-            // }
             return Ok(tokenResult.unwrap());
 
         } catch (error: any) {
@@ -67,10 +55,6 @@ export class AuthService {
             let isvalidTokenResult = await this.jwtService.verifyToken(token);
             if (isvalidTokenResult.isErr()) {
                 isvalidTokenResult = await this.oAuthService.verifyToken(token);
-                // if (isvalidTokenResult.isErr()) {
-                //     let { message } = isvalidTokenResult.unwrapErr();
-                //     return Err(new Error(message));
-                // }
             }
             return Ok(isvalidTokenResult.unwrap());
         } catch (error) {
@@ -90,10 +74,6 @@ export class AuthService {
     async callback(code: string): Promise<Ok<any> | Err<Error>> {
         try {
             const convertedTokenResult = await this.oAuthService.genrateTokenFromParam(code);
-            // if (convertedTokenResult.isErr()) {
-            //     const { message } = convertedTokenResult.unwrapErr();
-            //     return Err(new Error(message));
-            // }
             return Ok(convertedTokenResult.unwrap());
         } catch (error: any) {
             return Err(new Error(error.message));
